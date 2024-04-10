ab4beef2444f64d03b97a3246061dce5631a2d30
dinesh-gaire <132206290+dinesh-gaire@users.noreply.github.com>
2024-04-10 22:43:11 +0000

    nutritionist.py added- a nutrionist ai doctor

---
+++ nutritionist.py
@@ -0,0 +1,69 @@
+### AI Nutritionist
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load Google Gemini Pro Vision API And get response
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+##initialize our streamlit app
+
+st.set_page_config(page_title="AI Nutritionist")
+
+st.header("AI Nutritionist")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me the total calories")
+
+input_prompt="""
+You are an expert in nutritionist where you need to perform the following:
+1. Identify each food item.
+2. Estimate the calorie content for each identified food item.
+3. Provide a nutritional breakdown, including the percentage of carbohydrates, fats, fiber, and other nutrients.
+4. Assess whether each food item is generally considered healthy, based on its nutritional content.
+5. Summarize the total combined calorie count and overall healthiness of the meal.
+
+Please present the information in a structured format, with clear labels for each food item and its corresponding nutritional data.
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
+
